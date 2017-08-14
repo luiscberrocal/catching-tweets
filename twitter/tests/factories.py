c7aa93b8ef91d5af1acdfcdf06316a360b1e0681
LBerrocal <LBerrocal@pancanal.com>
2017-08-14 11:33:58 -0700

    Added tests for adapters

--- twitter/tests/factories.py
+++ twitter/tests/factories.py
@@ -2,6 +2,8 @@
 import string
 
 from random import randint
+from unittest.mock import Mock
+
 from pytz import timezone
 
 from django.conf import settings
@@ -38,3 +40,23 @@ class TweetFactory(DjangoModelFactory):
     user_screen_name = LazyAttribute(lambda x: faker.text(max_nb_chars=60))
     user_verified = Iterator([True, False])
 
+
+class MockTweetFactory(object):
+
+    def create(self,**kwargs):
+        mock_tweet = Mock()
+        mock_tweet.created_at = faker.date_time_between(start_date="-1y", end_date="now",
+                                                        tzinfo=timezone(settings.TIME_ZONE))
+        mock_tweet.favorite_count = 2
+        mock_tweet.id_str = FuzzyText(length=100, chars=string.digits).fuzz()
+        mock_tweet.source = faker.text(max_nb_chars=150)
+        mock_tweet.text = faker.text(max_nb_chars=150)
+        mock_tweet.user = Mock()
+        mock_tweet.user.description = faker.text(max_nb_chars=300)
+        mock_tweet.user.followers_count = 2
+        mock_tweet.user.id_str = FuzzyText(length=100, chars=string.digits).fuzz()
+        mock_tweet.user.location = faker.country()
+        mock_tweet.user.name = faker.user_name()
+        mock_tweet.user.screen_name = faker.name()
+        mock_tweet.user.verified = False
+        return mock_tweet
